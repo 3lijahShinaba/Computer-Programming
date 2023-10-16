ddd375603e03734502fdf45718d6793047e0257d
3lijahShinaba <148128799+3lijahShinaba@users.noreply.github.com>
2023-10-16 13:44:52 -0400

    Add files via upload

---
+++ GettingLoopy/src/GettingLoopy.java
@@ -0,0 +1,25 @@
+public class GettingLoopy {
+    public static void main(String[] args) {
+        // Task 1: Count up by 1 from 0 to 30
+        for (int i = 0; i <= 30; i++) {
+            System.out.println(i);
+        }
+        // Task 2: Count down by 1 from 30 to 0
+        for (int i = 30; i >= 0; i--) {
+            System.out.println(i);
+        }
+        // Task 3: Count up by 3 from 0 to 18
+        for (int i = 0; i <= 18; i += 3) {
+            System.out.println(i);
+        }
+        // Task 4: Count down by 2 from 10 to 0
+        for (int i = 10; i >= 0; i -= 2) {
+            System.out.println(i);
+
+
+        }
+
+
+    }
+}
+
---
+++ NestedLoops/src/Task07.java
@@ -0,0 +1,11 @@
+public class Task07 {
+    public static void main(String[] args) {
+        // Task 7: Nested loop to create a square of asterisks
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
